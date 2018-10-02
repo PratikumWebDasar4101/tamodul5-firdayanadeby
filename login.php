fa7392643ac99192199277e1c5755cd5f9acb4d2
firdayanadeby <43123859+firdayanadeby@users.noreply.github.com>
2018-10-02 14:43:43 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,19 @@
-<?php
-
-
-
-
+<?php 
+session_start();
+if (isset($_SESSION["username"])) {
+	header("location: registrasi.php");
+}
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form action="prosesLogin.php" method="POST">
+		<input type="text" name="username" placeholder="Username"><br>
+		<input type="password" name="password" placeholder="Password"><br>
+		<input type="submit" name="login" value="Login">
+	</form>
+</body>
+</html>
