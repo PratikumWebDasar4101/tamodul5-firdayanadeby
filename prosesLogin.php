62aee7a9a49600349134385654fe3c38ec96420c
firdayanadeby <43123859+firdayanadeby@users.noreply.github.com>
2018-10-02 14:43:04 +0100

    Update prosesLogin.php

--- prosesLogin.php
+++ prosesLogin.php
@@ -1,7 +1,32 @@
-<?php 
-
-
-
-
-
+<?php
+if(isset($_POST['login'])){
+	session_start();
+	$data = array(
+				array(
+					"id"=>"1",
+					"username"=>"admin",
+					"password"=>"admin",
+				)
+			);
+	if (isset($_POST['login'])) {
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+		$i = 0;
+		$data_valid = 0;
+		while($i < count($data)){
+			if($username == $data[$i]['username'] && $password == $data[$i]['password'] ){
+				$_SESSION['username'] = $username;
+				$_SESSION['akses'] = $data[$i]['akses'];
+				$_SESSION['id'] = $data[$i]['id'];
+				$data_valid = 1;
+			}
+			$i++;
+		}
+		if ($data_valid < 1) {
+			echo "Login Gagal";
+		} else {
+			header('location: registrasi.php');
+		}
+	}
+}
 ?>
